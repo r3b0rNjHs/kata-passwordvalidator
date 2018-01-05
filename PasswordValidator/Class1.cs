0b045fc083ef2302a728e4f7e319390301e3d189
Jorge <r3b0rn.jhs@gmail.com>
2018-01-05 17:24:28 +0000

    [Red] IsValid

--- PasswordValidator/Class1.cs
+++ PasswordValidator/Class1.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
 
 namespace PasswordValidator
 {
     public class PasswordValidatorShould
     {
 
+        [Fact]
+        public void isvalid()
+        {
+            Validate("Abc4_s").Should().BeTrue();
+        }
+
+        private bool Validate(string password)
+        {
+            return false;
+        }
     }
 }
